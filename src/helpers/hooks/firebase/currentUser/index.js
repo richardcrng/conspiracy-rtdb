e3330959fbb9405548d4d578e202d348c4cd4849
Richard Ng <richardcrng@gmail.com>
2019-05-21 20:00:33 +0100

    update useFirebaseCurrentUser

--- src/helpers/hooks/firebase/currentUser/index.js
+++ src/helpers/hooks/firebase/currentUser/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { useFirebase } from "use-firebase-context";
 
 function useFirebaseCurrentUser() {
+  const [user, setUser] = React.useState()
   const firebase = useFirebase()
-  return firebase.auth.currentUser
+
+  firebase.auth().onAuthStateChanged(setUser)
+
+  return user
 }
 
 export default useFirebaseCurrentUser;
