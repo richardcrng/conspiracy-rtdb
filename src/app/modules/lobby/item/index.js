7dd23bb5028f28c5981c98fc8a0ca58b9cae7542
Richard Ng <richardcrng@gmail.com>
2019-05-21 20:36:47 +0100

    add icon to LobbyItem

--- src/app/modules/lobby/item/index.js
+++ src/app/modules/lobby/item/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { MdExitToApp } from 'react-icons/md'
 
 function LobbyItem({ name }) {
   return (
-    <div><h2>{name}</h2></div>
+    <div className="d-flex justify-content-between" style={{ fontSize: "150%" }}>
+      <strong>{name}</strong>
+      <MdExitToApp size={30} />
+    </div>
   )
 }
 
