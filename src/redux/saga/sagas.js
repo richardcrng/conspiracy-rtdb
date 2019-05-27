ce5289fdbc12fa728df06eddb70e58b084aea60e
Richard Ng <richardcrng@gmail.com>
2019-05-27 20:36:38 +0100

    createGame saga

--- src/redux/saga/sagas.js
+++ src/redux/saga/sagas.js
@@ -3,6 +3,8 @@ import { all, select, call, getContext, takeLeading } from "redux-saga/effects";
 import selectors from "../selectors";
 import { makeActionCreator } from "redux-leaves";
 import { shouldBeConspiracy } from "../../helpers/conspiracy";
+import { generatePushID } from 'provide-firebase-middleware';
+import { references } from '../../firebase';
 
 export function* assignRoles() {
   const players = yield select(selectors.getGamePlayers)
@@ -21,6 +23,27 @@ export function* assignRoles() {
   }
 }
 
+export function* createGame({ payload: { host, name } }) {
+  const key = generatePushID()  // key for game
+  const firebase = yield getFirebase()
+
+  const gameConfig = { key, host, name, isInSignups: true }
+
+  if (host) {
+    gameConfig.players = {
+      [host]: {
+        key: host,
+        priority: generatePushID()
+      }
+    }
+    yield references.getPlayerByKey(host, firebase).update({ currentGame: key, isHost: true })
+  }
+
+  yield references.getGameByKey(key, firebase).update(gameConfig)
+
+  return key
+}
+
 export function* getFirebase() {
   return yield getContext('firebase')
 }
@@ -30,22 +53,22 @@ export function* startGame() {
   const gameId = yield select(selectors.getGameId)
   yield all([
     call(assignRoles),
-    firebase.database().ref(`games/${gameId}/isStarted`).set(true)
+    references.getGameByKey(gameId, firebase).update({ isStarted: true })
   ])
 }
 
 function* assignToAll(props = {}, playerIds = []) {
   const firebase = yield getFirebase()
   yield all(playerIds.map(id => (
-    firebase.database().ref(`players/${id}`).update(props)
+    references.getPlayerByKey(id, firebase).update(props)
   )))
 }
 
 function* setVictimOfGame(playerId, gameId) {
   const firebase = yield getFirebase()
   yield all([
-    firebase.database().ref(`players/${playerId}/isInnocent`).set(true),
-    firebase.database().ref(`games/${gameId}`).update({
+    references.getPlayerByKey(playerId, firebase).update({ isInnocent: true }),
+    references.getGameByKey(gameId, firebase).update({
       hasConspiracy: true,
       victim: playerId
     })
@@ -55,10 +78,14 @@ function* setVictimOfGame(playerId, gameId) {
 assignRoles.TRIGGER = "TRIGGER_SAGA: assignRoles"
 assignRoles.trigger = makeActionCreator(assignRoles.TRIGGER)
 
+createGame.TRIGGER = "TRIGGER_SAGA: createGame"
+createGame.trigger = makeActionCreator(createGame.TRIGGER)
+
 startGame.TRIGGER = "TRIGGER_SAGA: startGame"
 startGame.trigger = makeActionCreator(startGame.TRIGGER)
 
 export const sagas = [
   takeLeading(assignRoles.TRIGGER, assignRoles),
+  takeLeading(createGame.TRIGGER, createGame),
   takeLeading(startGame.TRIGGER, startGame)
 ]
