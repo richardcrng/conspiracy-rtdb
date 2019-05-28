02604237df05ab8320bb251d4c62221abcb1e5fa
Richard Ng <richardcrng@gmail.com>
2019-05-28 21:36:02 +0100

    trigger viewing result through button

--- src/app/modules/game/complete/GameComplete.js
+++ src/app/modules/game/complete/GameComplete.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import GameCompleteResult from './result';
+import { Button } from 'semantic-ui-react';
 
 function GameComplete() {
+  const [viewResult, setViewResult] = React.useState(false)
+
   return (
     <>
       <h1>Game complete</h1>
-      <GameCompleteResult />
+      {!viewResult && <GameCompleteView onClick={() => setViewResult(true)} />}
+      {viewResult && <GameCompleteResult />}
     </>
+  )
+}
+
+function GameCompleteView({ onClick }) {
+  return (
+    <Button primary onClick={onClick}>
+      View result
+    </Button>
   )
 }
 
