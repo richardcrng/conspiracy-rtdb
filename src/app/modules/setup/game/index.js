b73ec419104c952e24f584f30ac3ba188af899e9
Richard Ng <richardcrng@gmail.com>
2019-05-28 07:52:25 +0100

    implement useRouter instead of withRouter

--- src/app/modules/setup/game/index.js
+++ src/app/modules/setup/game/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { Button, Input } from 'semantic-ui-react';
 import { useFirebaseUserUid } from 'provide-firebase-middleware';
 import { createGame } from '../../../../redux/saga/sagas';
+import { useRouter } from '../../../providers/router/RouterProvider';
 
-function SetupGame({ history }) {
+function SetupGame() {
   const dispatch = useDispatch()
   const [name, setName] = React.useState("")
   const host = useFirebaseUserUid()
+  const { history } = useRouter()
 
   return (
     <>
@@ -27,6 +28,4 @@ function SetupGame({ history }) {
   )
 }
 
-// eslint-disable-next-line no-func-assign
-SetupGame = withRouter(SetupGame)
 export default SetupGame;
