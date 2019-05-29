e3991cc7f81c40ef2ea430bf403619bef606cdc7
Richard Ng <richardcrng@gmail.com>
2019-05-29 21:28:34 +0100

    refactor useGamePlayers to use better query

--- src/helpers/hooks/gamePlayers/useGamePlayers.js
+++ src/helpers/hooks/gamePlayers/useGamePlayers.js
@@ -3,21 +3,18 @@ import React from 'react';
 import { useFirebaseDatabaseValue, useStateHandlers } from 'provide-firebase-middleware';
 
 function useGamePlayers(gameId, asPrioritisedArray = false) {
-  const allPlayers = useFirebaseDatabaseValue('players')
-  const playersJoined = useFirebaseDatabaseValue(
-    `/games/${gameId}/players`,
-    { orderByChild: 'priority' }
-  )
+  const fromPlayers = useFirebaseDatabaseValue('players', {
+    orderByChild: "currentGame",
+    equalTo: gameId
+  })
+
+  const fromGame = useFirebaseDatabaseValue(`/games/${gameId}/players`,)
 
   const [gamePlayers, { set: setGamePlayers }] = useStateHandlers({})
   React.useEffect(() => {
-    const selectedPlayers = R.mapObjIndexed(
-      // merge to keep hold of properties specific to the game
-      ({ key }) => R.mergeLeft(R.prop(key, playersJoined))(R.prop(key, allPlayers)),
-      playersJoined
-    )
-    setGamePlayers(selectedPlayers)
-  }, [allPlayers, playersJoined, setGamePlayers])
+    const players = R.mergeDeepLeft(fromGame, fromPlayers)
+    setGamePlayers(players)
+  }, [fromPlayers, fromGame, setGamePlayers])
 
   return asPrioritisedArray
     ? R.sortBy(R.prop('priority'), Object.values(gamePlayers))
