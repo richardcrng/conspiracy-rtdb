e4a2bc68c5d095647b01e25ba8ec030fe1bd7d61
Richard Ng <richardcrng@gmail.com>
2019-05-29 20:24:14 +0100

    strip out firebase from LobbyItem

--- src/app/modules/lobby/index.js
+++ src/app/modules/lobby/index.js
@@ -1,35 +1,3 @@
-import _ from 'lodash';
-import React from 'react';
-import { useFirebaseDatabaseValue } from 'provide-firebase-middleware';
-import LobbyItem from './item';
-import { ROUTES } from '../../constants/routes';
-import LinkButton from '../../../lib/molecules/LinkButton';
-import ButtonCentreBottom from '../../../lib/molecules/ButtonCentreBottom';
+import Lobby from "./Lobby";
 
-function Lobby() {
-  const games = useFirebaseDatabaseValue('games')
-
-  return (
-    <>
-      <h1>Games available</h1>
-      {_.map(games, game => (
-        <LobbyItem
-          key={game.key}
-          id={game.key}
-          name={game.name}
-          players={game.players}
-        />
-      ))}
-      <div className="CenterBottom">
-        <ButtonCentreBottom
-          primary
-          size="huge"
-          text="Host new"
-          to={ROUTES.SetupGame}
-        />
-      </div>
-    </>
-  )
-}
-
-export default Lobby;
+export default Lobby
--- src/app/modules/lobby/item/index.js
+++ src/app/modules/lobby/item/index.js
@@ -1,27 +1,3 @@
-import * as R from 'ramda'
-import React from 'react';
-import { MdExitToApp } from 'react-icons/md'
-import { useFirebaseUserUid } from 'provide-firebase-middleware';
-import { useDispatch } from 'react-redux';
-import { joinGame } from '../../../../redux/saga/sagas';
+import LobbyItem from "./LobbyItem";
 
-function LobbyItem({ id, name, players }) {
-  const dispatch = useDispatch()
-  const uid = useFirebaseUserUid()
-
-  return (
-    <div className="d-flex justify-content-between" style={{ fontSize: "150%" }}>
-      <strong>{name}</strong>
-      <MdExitToApp
-        onClick={() => {
-          if (uid && !R.prop(uid, players)) {
-            dispatch(joinGame.trigger({ playerKey: uid, gameKey: id }))
-          }
-        }}
-        size={30}
-      />
-    </div>
-  )
-}
-
-export default LobbyItem;
+export default LobbyItem
