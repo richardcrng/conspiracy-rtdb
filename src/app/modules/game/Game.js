73f6c47edb6d8198706bca5525a78461a0c2f743
Richard Ng <richardcrng@gmail.com>
2019-05-29 18:59:26 +0100

    refactor Game component

--- src/app/modules/game/Game.js
+++ src/app/modules/game/Game.js
@@ -1,26 +1,18 @@
-import * as R from 'ramda'
 import React from 'react';
-import { useFirebaseDatabaseValue } from 'provide-firebase-middleware';
-import { useDispatch, useSelector } from 'react-redux';
-import { actions } from '../../../redux/leaves';
+import { useSelector } from 'react-redux';
 import selectors from '../../../redux/selectors';
 import GamePrestart from './prestart';
-import useGamePlayers from '../../../helpers/hooks/gamePlayers';
 import GameOngoing from './ongoing';
 import GameComplete from './complete';
-import { updatePlayer } from '../../../redux/saga/sagas';
 
 function Game() {
   const isComplete = useSelector(selectors.getGameIsComplete)
   const isStarted = useSelector(selectors.getGameIsStarted)
 
-  if (isComplete) {
-    return <GameComplete />
-  } else if (isStarted) {
-    return <GameOngoing />
-  } else return (
-    <GamePrestart />
-  )
+  if (isComplete) return <GameComplete />
+  if (isStarted) return <GameOngoing />
+  
+  return <GamePrestart />
 }
 
 export default Game;
