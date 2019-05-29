56306debc27faf455788429c58c73ca12402bfda
Richard Ng <richardcrng@gmail.com>
2019-05-29 19:01:18 +0100

    remove push in LobbyItem

--- src/app/modules/lobby/item/index.js
+++ src/app/modules/lobby/item/index.js
@@ -4,12 +4,10 @@ import { MdExitToApp } from 'react-icons/md'
 import { useFirebaseUserUid } from 'provide-firebase-middleware';
 import { useDispatch } from 'react-redux';
 import { joinGame } from '../../../../redux/saga/sagas';
-import { useRouter } from '../../../providers/router/RouterProvider';
 
 function LobbyItem({ id, name, players }) {
   const dispatch = useDispatch()
   const uid = useFirebaseUserUid()
-  const { history } = useRouter()
 
   return (
     <div className="d-flex justify-content-between" style={{ fontSize: "150%" }}>
@@ -19,7 +17,6 @@ function LobbyItem({ id, name, players }) {
           if (uid && !R.prop(uid, players)) {
             dispatch(joinGame.trigger({ playerKey: uid, gameKey: id }))
           }
-          history.push(`/game/${id}/players`)
         }}
         size={30}
       />
