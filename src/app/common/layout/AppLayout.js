4f7bc9f36e19ba503282cd290ab2fe15e6d94d7d
Richard Ng <richardcrng@gmail.com>
2019-05-30 21:08:44 +0100

    create signout button in navbar

--- src/app/common/layout/AppLayout.js
+++ src/app/common/layout/AppLayout.js
@@ -3,13 +3,17 @@ import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../constants/routes';
 import classes from './AppLayout.module.css';
+import { useFirebase, useFirebaseUser } from 'provide-firebase-middleware';
 
 function AppLayout({ children }) {
+  const user = useFirebaseUser()
+
   return (
     <>
       <Navbar bg="dark" variant="dark" >
         <Link to="/"><Navbar.Brand>Conspiracy</Navbar.Brand></Link>
         <NavItem to={ROUTES.SetupProfile} text="Change name" />
+        {user && <SignOut />}
         {/* <NavItem to={ROUTES.Lobby} text="Lobby" /> */}
         {/* <NavItem to={ROUTES.Game} text="Game" /> */}
       </Navbar>
@@ -26,6 +30,19 @@ function NavItem({ to, text }) {
       <Link to={to} >
         <Navbar.Text>{text}</Navbar.Text>
       </Link>
+    </div>
+  )
+}
+
+function SignOut() {
+  const firebase = useFirebase()
+
+  return (
+    <div
+      className="mx-auto"
+      onClick={() => firebase.auth().signOut()}
+    >
+      <Navbar.Text>Sign out</Navbar.Text>
     </div>
   )
 }
