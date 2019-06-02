1a8b5e853cb91e34119dd3689ca61bf3fe79de07
Richard Ng <richardcrng@gmail.com>
2019-06-02 19:12:36 +0100

    redirect from navbar to root

--- src/app/modules/setup/game/SetupGame.js
+++ src/app/modules/setup/game/SetupGame.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createGame } from '../../../../redux/saga/sagas';
-import ButtonCentreBottom from '../../../lib/molecules/ButtonCentreBottom';
-import selectors from '../../../../redux/selectors';
 import LinkButton from '../../../lib/molecules/LinkButton';
 import { ROUTES } from '../../../constants/routes';
-import { IonInput, IonLabel } from '@ionic/react';
 import SetupGameForm from './form';
 
 function SetupGame() {
-  // const dispatch = useDispatch()
-  // const [name, setName] = React.useState("")
-  // const host = useSelector(selectors.getUserKey)
-
   return (
     <>
       <h1>Host New Game</h1>
