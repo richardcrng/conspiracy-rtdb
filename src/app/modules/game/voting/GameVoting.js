12a810f1d44026297bf71d0dca6935b70a9ff862
Richard Ng <richardcrng@gmail.com>
2019-06-10 14:26:28 +0100

    create button to return to role

--- src/app/modules/game/voting/GameVoting.js
+++ src/app/modules/game/voting/GameVoting.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import GameVotingTable from './table';
 import GameVotingVote from './vote';
+import LinkButton from '../../../lib/molecules/LinkButton';
+import { ROUTES } from '../../../constants/routes';
 
 function GameVoting() {
   return (
     <>
+      <div className="mb-3">
+        <LinkButton
+          basic
+          fill="outline"
+          text="View role"
+          to={ROUTES.GameRole}
+        />
+      </div>
       <GameVotingTable />
       <GameVotingVote />
     </>
